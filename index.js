22d70f84237fcfebbff4af0297ef815622fd036f
Vansh Gupta <vg3994284@gmail.com>
2024-08-21 10:14:16 +0530

    bugs fixed

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const PORT = 3000;
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.set("view engine", "ejs");
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
@@ -21,6 +25,7 @@ io.on("connection", (socket) => {
     console.log("User connected id: ", socket.id);
 })
 
+// Route to render index.ejs
 app.get("/", (req, res) => {
     res.render("index");
 })

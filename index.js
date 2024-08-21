bb1dc3487936e1605bd79c9cdfe62ebfd4445798
Vansh Gupta <vg3994284@gmail.com>
2024-08-21 10:18:52 +0530

    Code updated

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const io = socketio(server);
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
